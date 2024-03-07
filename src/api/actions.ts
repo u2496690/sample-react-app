06190621c7c65f9a02fc12489c57de44e8030041
u2496690 <147236980+u2496690@users.noreply.github.com>
2024-03-07 15:08:32 +0000

    working frontend

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https://literate-space-engine-69g6w5jjj456c5jv7-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
